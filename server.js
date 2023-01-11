7354290ebeb1cadaf0fcd35fc897ee08e402039d
Raphaël Grand <raphael.grand@zenika.com>
2023-01-11 10:47:40 +0100

    Test server

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-import express from "express";
+ import express from "express";
+import http from "http";
 import cors from "cors";
 import "dotenv/config";
+import { Server } from "socket.io";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+ 
 const port = process.env.PORT || 8080;
-app.use(cors({ origin: process.env.ORIGIN }));
+const teams = [];
+const buzzes = [];
 
-app.listen(port, () => {
-  console.log(`Running server on port ${port}`);
-})
+app.use(cors({ origin: "https://lne0nl.github.io/" }));
+
+io.on("connect", (socket) => {
+  console.log(`user ${socket.id} connected`);
+
+  io.emit("team-added", teams);
+
+  socket.on("add-team", (team) => {
+    teams.push(team);
+    io.emit("team-added", teams);
+    console.log("New team added => ", teams);
+  });
+
+  socket.on("buzz", (teamName) => {
+    if (!buzzes.includes(teamName)) buzzes.push(teamName);
+    if (buzzes.length >= 1) io.emit("buzz-win", buzzes[0]);
+    console.log(buzzes.length)
+
+    console.log(`l'équipe ${teamName} a buzzé !`)
+    console.log(buzzes)
+  })
+});
+
+server.listen(port, () => console.log(`Running server on port ${port}`));
